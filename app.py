f987d5c7b3392f98cdf5fcf0989a569bb9e1ae4e
Nathan <nathan_allen@sweetwater.com>
2021-08-06 15:11:50 -0400

    commit on main

--- app.py
+++ app.py
@@ -4,36 +4,48 @@ import pandas
 import random
 
 app = Flask(__name__)
+path = "./wordFiles/"
+
+def get_word(type = 'noun'):
+    """Get a random word of the requested type
+    
+    Keyword Arguments:
+        type {string} -- The part of speech you want to return. Options = word, noun, adverb, adjective, animal, special, verb, prepostion, special
+    
+    Returns:
+        [string] -- word of the requested type
+    """
+
+    words = pandas.read_csv(path+type+'.csv',header=0)
+    wordCount = words.count(0).values[0]
+    word = words.iloc[random.randint(0,wordCount-1)].values[0]
+
+    return word
 
 @app.route("/")
 def main():
-    adverbs = pandas.read_csv('adverbs.csv',header=0)
-    adjectives = pandas.read_csv('adjectives.csv',header=0)
-    animals = pandas.read_csv('animals.csv',header=0)
-    specials = pandas.read_csv('special.csv',header=0)
-
-    adverbCount = adverbs.count(0).values[0]
-    adjectiveCount = adjectives.count(0).values[0]
-    animalCount = animals.count(0).values[0]
-    specialCount = specials.count(0).values[0]
-
-    selectAdverb = random.randint(0,adverbCount-1)
-    selectAdjective = random.randint(0,adjectiveCount-1)
-    selectAnimal = random.randint(0,animalCount-1)
-    selectSpecial = random.randint(0,specialCount-1)
-
-    adverb = adverbs.iloc[selectAdverb].values[0]
-    adjective = adjectives.iloc[selectAdjective].values[0]
-    animal = animals.iloc[selectAnimal].values[0]
+    adverb = get_word(type='adverb')
+    adjective = get_word(type='adjective')
+    animal = get_word(type='animal')
     number = random.randint(0,9)
-    special = specials.iloc[selectSpecial].values[0]
-
+    special = get_word(type='special')
     password = adverb + adjective + animal + str(number) + special
 
     return render_template(
-        'index.html', **locals())
+        'index.html', **locals()
+        )
+
+@app.route('/words/')
+def nouns():
+    i = 1
+    password = ''
+    while i < 5:
+        password = password + get_word(type='word').capitalize()
+        i += 1
+    
+    return render_template(
+        'index.html', **locals()
+        )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
-
